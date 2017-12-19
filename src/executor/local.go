4f3d8eca240c6a0784be141bdb6e4d8437da021f
evgeny.nefedkin <evgeny.nefedkin@umbrella-web.com>
2017-12-19 17:33:53 +0300

    Review 0.20 (fix: starting job with silent:true)

--- src/executor/local.go
+++ src/executor/local.go
@@ -81,6 +81,12 @@ func (a *localExecutor)ExecItem(job hrentabd.Job, wg *sync.WaitGroup) (out []byt
 	// RUN COMMAND
 	if cmd := exec.Command("sh",  "-c", job.Command()); !a.silent {
 		out,err = cmd.Output()
+	}else{
+		if a.async{
+			cmd.Start()
+		}else{
+			cmd.Run()
+		}
 	}
 
 	// on item complete
