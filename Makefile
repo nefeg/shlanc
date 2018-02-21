46a7d6d1217eb76e90c748f677d7d17b80a68bc2
evgeny.nefedkin <evgeny.nefedkin@umbrella-web.com>
2018-02-21 20:25:03 +0300

    Configure Makefile for PPA

--- Makefile
+++ Makefile
@@ -1,58 +1,37 @@
-#SHELL = /bin/bash
-#PREFIX = /usr/local/bin
-#GOPATH = $(shell pwd)
-#GOBIN = $(GOPATH)/bin
-#TARGET1 = shlancd
-#TARGET2 = shlanc
-#CONF = /etc/shlanc
-#
-#
+SHELL = /bin/bash
+BIN	= $(DESTDIR)/usr/bin
+GOPATH = $(shell pwd)
+GOBIN = $(GOPATH)/bin
+TARGET1 = shlancd
+TARGET2 = shlanc
+CONF = /etc/shlanc
+
+
 ## These will be provided to the target
-#VERSION := 1.0.0
-#BUILD := `git rev-parse HEAD`
+VERSION := 1.0.0
+BUILD := `date`
 #
 ## Use linker flags to provide version/build settings to the target
-#LDFLAGS=-ldflags "-X=main.Version=$(VERSION) -X=main.Build=$(BUILD)"
-#
-#.PHONY: all install uninstall
-#
-#all:
-#	mkdir -p $(GOBIN)
-#	GOPATH=$(GOPATH) GOBIN=$(GOBIN) go get .
-#	GOPATH=$(GOPATH) GOBIN=$(GOBIN) go install $(LDFLAGS) $(TARGET1) $(TARGET2)
-#
-#install:
-#	install $(GOBIN)/$(TARGET1) $(PREFIX)
-#	install $(GOBIN)/$(TARGET2) $(PREFIX)
-#
-#	@mkdir $(CONF) -p
-#	install ./config.json $(CONF)/config.json
-#
-#uninstall:
-#	rm -rf $(PREFIX)/$(TARGET1)
-#	rm -rf $(PREFIX)/$(TARGET2)
-#	rm -rf $(CONF)/config.json
-
-
-TARGET = shlancd
-BIN	= $(DESTDIR)/usr/bin
-CONF = /etc/shlancd
-
-# These will be provided to the target
-VERSION := 1.0.0
-BUILD := `git rev-parse HEAD`
-
-# Use linker flags to provide version/build settings to the target
 LDFLAGS=-ldflags "-X=main.Version=$(VERSION) -X=main.Build=$(BUILD)"
 
-.PHONY: install uninstall
+.PHONY: all install uninstall
+
+all:
+	mkdir -p $(GOBIN)
+	@echo " * load dependensies"
+	GOPATH=$(GOPATH) GOBIN=$(GOBIN) go get ./...
+
+	@echo " * compile binaries"
+	GOPATH=$(GOPATH) GOBIN=$(GOBIN) go install $(LDFLAGS) $(TARGET1) $(TARGET2)
 
 install:
-	install ./bin/$(TARGET) $(BIN)/$(TARGET)
+	install $(GOBIN)/$(TARGET1) $(BIN)
+	install $(GOBIN)/$(TARGET2) $(BIN)
 
 	@mkdir $(CONF) -p
 	install ./config.json $(CONF)/config.json
 
 uninstall:
-	rm -rf $(BIN)/$(TARGET)
+	rm -rf $(BIN)/$(TARGET1)
+	rm -rf $(BIN)/$(TARGET2)
 	rm -rf $(CONF)/config.json
---
+++ build_deb.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+# project dir
+#   |_src
+#   |_WORK_DIR
+#       |_TMP_DIR
+#       |_BUILD_NAME.tar.gz
+#
+#
+#
+VERSION="0.25-0ubuntu1ppa1~trusty"
+BUILD_NAME="shlanc-${VERSION}"
+BUILD_NAME_ALT="shlanc_${VERSION}"
+WORK_DIR=$(pwd)"/build"
+TMP_DIR=$WORK_DIR/tmp
+
+export DEBEMAIL="evgeny.nefedkin@umbrella-web.com"
+export DEBFULLNAME="Evgeny Nefedkin"
+
+mkdir $TMP_DIR -p
+git archive --format=tar.gz -o $WORK_DIR/$BUILD_NAME.tar.gz ppa
+
+
+cd $TMP_DIR;
+dh_make -f ../$BUILD_NAME.tar.gz -s -e evgeny.nefedkin@umbrella-web.com -c gpl2 -y --createorig -p $BUILD_NAME_ALT
+
+###################################
+#   edit 'debian/control' file    #
+###################################
+sed -i 's/#Vcs-Browser: http:\/\/git.debian.org\/?p=collab-maint\/shlanc.git;a=summary/Vcs-Browser: https:\/\/verefkin@bitbucket.org\/verefkin\/hrentab.git/' debian/control
+sed -i 's/#Vcs-Git: git:\/\/git.debian.org\/collab-maint\/shlanc.git/Vcs-Git: https:\/\/verefkin@bitbucket.org\/verefkin\/hrentab.git/' debian/control
+sed -i 's/<insert the upstream URL, if relevant>/https:\/\/verefkin@bitbucket.org\/verefkin\/hrentab.git/' debian/control
+#sed -ri 's/^Depends: .*/Depends: /' debian/control
+sed -i 's/<insert up to 60 chars description>/Distributed and concurrency manager of deffer jobs/' debian/control
+sed -i 's/<insert long description, indented with spaces>/Distributed and concurrency manager of deffer jobs and something else/' debian/control
+sed -i 's/Section: unknown/Section: utils/' debian/control
+
+
+###################################
+#   edit 'debian/copyright' file  #
+###################################
+sed -i 's/<url:\/\/example.com>/https:\/\/verefkin@bitbucket.org\/verefkin\/hrentab.git/' debian/copyright
+sed -i "s/<years> <put author.s name and email here>/${DEBFULLNAME} <${DEBEMAIL}>/" debian/copyright
+sed -i 's/<years> <likewise for another author>//' debian/copyright
+sed -ri 's/^#.+//' debian/copyright
+
+###################################
+#   edit 'debian/changelog' file  #
+###################################
+sed -i 's/unstable;/trusty;/' debian/changelog
+vi debian/changelog
+
+
+## Remove useless files
+rm debian/*.ex
+rm debian/*.EX
+
+## build package (https://help.launchpad.net/Packaging/PPA/BuildingASourcePackage)
+debuild -S -sa
+
+#dput ppa:onm/shlac ${BUILD_NAME_ALT}-1_source.changes -d
+
+#rm -rf $WORK_DIR
+
